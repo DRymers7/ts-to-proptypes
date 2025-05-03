7b1444a0078703cf22e8f9b93754a9ac23a4a42b
Derek Rymers <rymersd@gmail.com>
2025-05-03 16:32:44 -0400

    normalized prop type

--- src/types.ts
+++ src/types.ts
@@ -20,3 +20,16 @@ export interface WriteOptions {
     inline?: boolean;
     prettier?: boolean;
 }
+
+/**
+ * Type representation for ts-to-proptypes to standardize the AST node value to a generic
+ * constant, and then use that value to generate a new tsx prop.
+ */
+export type NormalizedPropType =
+  | { kind: 'primitive'; name: 'string' | 'number' | 'boolean' }
+  | { kind: 'array' }
+  | { kind: 'object' }
+  | { kind: 'function' }
+  | { kind: 'oneOf'; values: Array<string | number | boolean> }
+  | { kind: 'oneOfType'; types: NormalizedPropType[] }
+  | { kind: 'any' };
